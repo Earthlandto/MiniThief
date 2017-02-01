f6c6eaf55661978c12d956ee46dc83e7bc3c24bd
Adrián Egea Comenge <adrian.egea.comenge@gmail.com>
2017-02-01 23:21:45 +0100

    Complete doc.

--- minithief.js
+++ minithief.js
@@ -21,11 +21,16 @@ Point.prototype.constructor = Point;
 //----------------------------------------------------------------------
 
 /**
- * Enum for shape values.
+ * @class
+ * @classdesc Enum for shape values.
  * @enum {number}
  */
 var Shape = {
+  /** @member {number} */
+  /** Rectangular shape.*/
   RECTANGLE : 0,
+  /** @member {number} */
+  /** Circular shape.*/
   CIRCLE : 1
 };
 
@@ -37,6 +42,14 @@ var Shape = {
 * @extends {Script}
 * @classdesc This class represents a behaviour for a GameElement.
 * @param {Object} param The parameters of the behaviour.
+* The object param contains these callbacks:
+<ul>
+<li><strong>start</strong> : This function runs when the engine start at first time.</li>
+<li><strong>update</strong> : This function runs each frame.</li>
+<li><strong>onEnterCollision</strong> : This function runs when the game element collides with other game element.</li>
+<li><strong>onExitCollision</strong> : This function runs when the collision is finished.</li>
+<li><strong>onDestroy</strong> : This function runs when the game element is destroyed.</li>
+</ul>
 */
 var Behaviour = function (param) {
     Script.call(this);
@@ -118,6 +131,18 @@ Behaviour.KEY_CONTROL = {
 * @extends {Script}
 * @classdesc This class represents a GameElement.
 * @param {Object} param The parameters of the gameelement.
+* The object param contains these fields:
+<ul>
+<li><strong>position</strong> : {Point} The position.</li>
+<li><strong>texture</strong> : {String} The name of the texture.</li>
+<li><strong>width</strong> : {Number} The width.</li>
+<li><strong>height</strong> : {Number} The height.</li>
+<li><strong>shape</strong> : {Shape} The shape.</li>
+<li><strong>behaviour</strong> : {Object} The behaviour's parameters.</li>
+<li><strong>isStatic</strong> : {Bool} True if static.</li>
+<li><strong>isSensor</strong> : {Bool} True if sensor.</li>
+<li><strong>name</strong> : {String} The name of the game element.</li>
+</ul>
 */
 var GameElement = function (param) {
 
@@ -161,22 +186,42 @@ var GameElement = function (param) {
 
 };
 
+/**
+* Returns the name of the game element.
+* @returns {String} The name.
+*/
 GameElement.prototype.getName = function () {
   return this.gameObject.getName();
 };
 
+/**
+* Returns the position of the game element.
+* @returns {Point} The name.
+*/
 GameElement.prototype.getPosition = function () {
   return this.transform.getPosition();
 };
 
+/**
+* Moves the game element to a position.
+* @param {Point} position The new position.
+*/
 GameElement.prototype.moveTo = function (position) {
   this.body.getBox2dBody().SetPosition(new b2Vec2(position.x,position.y));
 };
 
+/**
+* Applies a force to the game element.
+* @param {Point} vector The force vector.
+*/
 GameElement.prototype.applyForce = function (vector) {
   this.body.getBox2dBody().ApplyForce(new b2Vec2(vector.x,vector.y),this.body.GetWorldCenter());
 };
 
+/**
+* Sets the velocity of the game element.
+* @param {Point} vector The velocity vector.
+*/
 GameElement.prototype.setVelocity = function (vector) {
 
   var linear = this.body.getBox2dBody().GetLinearVelocity();
@@ -185,16 +230,27 @@ GameElement.prototype.setVelocity = function (vector) {
   linear.y = vector.y;
 };
 
-GameElement.prototype.rotate = function () {
+/**
+* Rotates the game element.
+* @param {number} angle The angle in degrees.
+*/
+GameElement.prototype.rotate = function (angle) {
 
 };
 
+/**
+* Adds a new behaviour to the game element.
+* @param {Object} behaviourParam The behaviour's parameters.
+*/
 GameElement.prototype.addBehaviour = function (behaviourParam) {
   var b = new Behaviour(behaviourParam);
   b.gameElement = this;
   this.gameObject.addComponent(b);
 };
 
+/**
+* Destroys the game element.
+*/
 GameElement.prototype.destroy = function () {
   this.gameObject.destroy();
 };
@@ -205,10 +261,16 @@ GameElement.prototype.destroy = function () {
 
 //----------------------------------------------------------------------
 
+/**
+* @namespace
+*/
 var Game = {}; // namespace
 
 //----------------------------------------------------------------------
 
+/**
+* Initializes the engine.
+*/
 Game.init = function () {
   // inicializa motor
 
@@ -239,28 +301,60 @@ Game.init = function () {
 
 //----------------------------------------------------------------------
 
+/**
+* Starts the engine.
+*/
 Game.run = function () {
   Thief.run();
 };
 
 //----------------------------------------------------------------------
-
+/**
+* Adds a game element to the the engine.
+* @param {GameElement} elem The game element.
+*/
 Game.add = function(elem){
   Thief.addGameObjectToScene(elem.gameObject);
 };
 
 //----------------------------------------------------------------------
 
+/**
+* @namespace
+*/
 Game.create = {}; // namespace
 
 //----------------------------------------------------------------------
 
+/**
+* Returns a game element.
+* @param {Object} param The parameters of the gameelement.
+* The object param contains these fields:
+<ul>
+<li><strong>position</strong> : {Point} The position.</li>
+<li><strong>texture</strong> : {String} The name of the texture.</li>
+<li><strong>width</strong> : {Number} The width.</li>
+<li><strong>height</strong> : {Number} The height.</li>
+<li><strong>shape</strong> : {Shape} The shape.</li>
+<li><strong>behaviour</strong> : {Object} The behaviour's parameters.</li>
+<li><strong>isStatic</strong> : {Bool} True if static.</li>
+<li><strong>isSensor</strong> : {Bool} True if sensor.</li>
+<li><strong>name</strong> : {String} The name of the game element.</li>
+</ul>
+* @returns {GameElement} The game element.
+*/
 Game.create.gameElement = function (param){
   return new GameElement(param);
 };
 
 //----------------------------------------------------------------------
 
+/**
+* Returns a tile.
+* @param {Point} position The position.
+* @param {String} texture The name of the texture.
+* @returns {GameElement} The tile.
+*/
 Game.create.tile = function (position,texture){
   return Game.create.gameElement({
     position : position,
