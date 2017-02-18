68c42ba976111a1adbcd85f09494a3a379e4b00f
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2017-02-18 20:13:19 +0100

    Prettified files

--- scripts/mygame.js
+++ scripts/mygame.js
@@ -1,39 +1,40 @@
-var run = function () {
+var run = function() {
 
   Game.init();
 
   var mario1 = Game.create.gameElement({
-    name : "player",
-    position : new Point(2,2),
-    texture : "images/mario.png",
-    width : 1,
-    height : 1,
-    shape : Shape.RECTANGLE,
-    behaviour : Behaviour.KEY_CONTROL,
-    isStatic : false,
-    isSensor : false
+    name: "player",
+    position: new Point(2, 2),
+    texture: "images/mario.png",
+    width: 1,
+    height: 1,
+    shape: Shape.RECTANGLE,
+    behaviour: Behaviour.KEY_CONTROL,
+    isStatic: false,
+    isSensor: false
   });
 
   var mario2 = Game.create.gameElement({
-    texture : "images/mario.png",
-    behaviour : {
-      onEnterCollision: function (otherName) {
-        if(otherName == "player")
+    texture: "images/mario.png",
+    behaviour: {
+      onEnterCollision: function(otherName) {
+        if (otherName == "player") {
           this.gameElement.destroy();
+        }
       }
     }
   });
 
   var mario3 = Game.create.gameElement({
-    position : new Point(-2,2),
-    texture : "images/mario.png"
+    position: new Point(-2, 2),
+    texture: "images/mario.png"
   });
 
 
   var size = 20;
-  for (var i = -(size/2); i < size/2; i++) {
-    for (var j = -(size/2); j < size/2; j++) {
-      Game.add(Game.create.tile(new Point(i,j),"images/sand.png"));
+  for (var i = -(size / 2); i < size / 2; i++) {
+    for (var j = -(size / 2); j < size / 2; j++) {
+      Game.add(Game.create.tile(new Point(i, j), "images/sand.png"));
     }
   }
 
